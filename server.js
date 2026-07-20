3224ba98aceabd3770615cb4dbbb6c7625a90f7e
luisaromero <luisa.marcela.romero@gmail.com>
2026-07-20 05:20:11 -0400

    organizing folders

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const path = require('path');
+
 const cors = require('cors');
 const { leerMascotas, escribirMascotas } = require('./db');
 const { validarRut } = require('./rut');
@@ -8,6 +10,8 @@ const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 // ------------------------------------------------------------------
 // Helpers
